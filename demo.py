cce2d4446c0169f8f95c3d10f02ac0206e58a6ef
linyq <linyqemail@163.com>
2024-08-02 11:30:54 +0800

    1212

--- demo.py
+++ demo.py
@@ -5,5 +5,10 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
+
 arr = [5, 3, 8, 1, 9, 2]
 print(bubble_sort(arr))
+
+if __name__ == '__main__':
+    print(bubble_sort([5, 3, 8, 1, 9, 2]))
