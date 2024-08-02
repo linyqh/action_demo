6375a491062a14aff3785cbbb906b6cc99a77c38
linyq <linyqemail@163.com>
2024-08-02 11:28:55 +0800

    123

---
+++ demo.py
@@ -0,0 +1,9 @@
+#  写一个冒泡排序
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+arr = [5, 3, 8, 1, 9, 2]
+print(bubble_sort(arr))
